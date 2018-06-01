41728fa310a5135cffd621f8a4d475f4fec8effc
AColossus <842236625@qq.com>
2018-06-01 15:55:14 +0800

    完善了组件

--- src/main/webapp/resources/js/personalZone.js
+++ src/main/webapp/resources/js/personalZone.js
@@ -3,7 +3,7 @@ const projects=new Vue({
     data:{
         projects:[
             {name:'原生体',time:'2018-05-06',preview_url:'../resources/images/desk.jpeg'},
-            {name:'完美世界',time:'2018-05-06',preview_url:'../resources/images/star.jpg'},
+            {name:'完美世界',time:'2018-05-06',preview_url:'../resources/images/star.jpeg'},
             {name:'原生体',time:'2018-05-06',preview_url:'../resources/images/1.jpeg'},
             {name:'原生体',time:'2018-05-06',preview_url:'../resources/images/2.jpeg'},
             {name:'原生体',time:'2018-05-06',preview_url:'../resources/images/3.jpeg'},
