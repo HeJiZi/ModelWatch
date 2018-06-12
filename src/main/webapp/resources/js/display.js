8ac85ff3f9286d75c8921dd4df3befc39924c61c
AColossus <842236625@qq.com>
2018-06-12 21:57:13 +0800

    完成了旋转观看功能

--- src/main/webapp/resources/js/display.js
+++ src/main/webapp/resources/js/display.js
@@ -1,4 +1,8 @@
-
+var renderer
+var camera
+var scene
+var light
+var mesh
 const viewapp = new Vue({
     el: '#view',
     data: {
@@ -22,10 +26,10 @@ const viewapp = new Vue({
             renderer.setSize(window.innerWidth, window.innerHeight);
             renderer.render(scene, camera)
         }
+        document.oncontextmenu=function(){return false;}
     },
     mounted: function () {
         this.init3D();
-
     },
     computed: {
         switch_h: function () {
@@ -40,22 +44,19 @@ const viewapp = new Vue({
             window.location.href = '../';
         },
         init3D: function () {
-            
+
             initThree(this);
             initCamera();
             initScene();
             initLight();
             initObject();
-
+            renderer.render(scene, camera)
         },
-        pushControl: function (event) {
-            console.log(event.button)
-        }
     }
 })
-var renderer;
+
 function initThree(obj) {
-    
+
     width = window.innerWidth;
     height = window.innerHeight
     renderer = new THREE.WebGLRenderer({
@@ -66,36 +67,85 @@ function initThree(obj) {
     renderer.setClearColor(0xF0F0F0, 1.0);
 }
 
-var camera;
+
 function initCamera() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.x = 600;
-    camera.position.y = 0;
-    camera.position.z = 600;
+    camera.position.x = 1500;
+    camera.position.y = 500;
+    camera.position.z = 1000;
     camera.up.x = 0;
     camera.up.y = 1;
     camera.up.z = 0;
     camera.lookAt(0, 0, 0);
 }
 
-var scene;
+
 function initScene() {
     scene = new THREE.Scene();
 }
 
-var light;
+
 function initLight() {
-    light = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+    light = new THREE.DirectionalLight(0xFFFFFF, 1);
     // 位置不同，方向光作用于物体的面也不同，看到的物体各个面的颜色也不一样
     light.position.set(0, 0, 1);
     scene.add(light);
 }
 
-var cube;
 function initObject() {
     var geometry = new THREE.CubeGeometry(200, 100, 50);
     var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position = new THREE.Vector3(0, 0, 0);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.y += 51;
+
+    var g2 = new THREE.PlaneGeometry(800, 1000, 3);
+    var ma = new THREE.MeshBasicMaterial({ color: 0xCFCFCF, side: THREE.DoubleSide })
+    ma.transparent = true;
+    ma.opacity = 0.2
+    var plane = new THREE.Mesh(g2, ma)
+    plane.rotateX(3.14 / 2)
+
+    var g3 = new THREE.CubeGeometry(50, 50, 50);
+    var m3 = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
+    sphere = new THREE.Mesh(g3, m3);
+    sphere.position.x = 240
+    sphere.position.z = 200;
+    sphere.position.y = 80
+    sphere.up.x = 0;
+    sphere.up.y = 1;
+    sphere.up.z = 0;
+
     scene.add(mesh);
+    scene.add(plane)
+    scene.add(sphere)
+}
+
+function pushControl(event) {
+    var temp = document.getElementById('3dcanvas')
+    x = event.clientX
+    y = event.clientY
+    var bcode = event.button
+    console.log(bcode)
+    switch (bcode) {
+        case 0: 
+            temp.onmousemove = function (event) {
+                mouseSurround(event, mesh)
+            }
+            break;
+        case 2:
+            temp.onmousemove=function(event){
+                mouseTranslate(event,mesh)
+            }
+        
+    }
+
+}
+function upControl(event) {
+    var temp = document.getElementById('3dcanvas')
+    temp.onmousemove = null;
+
+}
+function moveclean() {
+    var target = document.getElementById('3dcanvas')
+    target.onmousemove = null;
 }
---
+++ src/main/webapp/resources/js/modelControl.js
@@ -0,0 +1,62 @@
+function mouseSurround(event,target){
+    var dx=event.clientX-x;
+    var dy=event.clientY-y;
+    x=event.clientX
+    y=event.clientY
+
+    SurroundY(camera,target.position,dx/100.0)
+    SurroundZ(camera,target.position,-dy/100.0)
+
+    renderer.render(scene,camera)
+}
+
+function mouseTranslate(event,target){
+    
+}
+
+
+function SurroundY(obj,target,angle){
+
+    
+    //转换坐标系
+    turnLocal(obj,target)
+    var after=obj.position.y
+    var m=new THREE.Matrix3();
+    m.set(Math.cos(angle),0,-Math.sin(angle),
+            0,1,0,
+            Math.sin(angle),0,Math.cos(angle))
+    obj.position.applyMatrix3(m)
+
+
+    //坐标转换回世界坐标系
+    turnWorld(obj,target)
+    obj.lookAt(target)
+
+
+}
+function SurroundZ(obj,target,angle){
+    
+    
+    //转换坐标系
+    turnLocal(obj,target)
+    var m=new THREE.Matrix3();
+    m.set( Math.cos(angle),Math.sin(angle),0,
+            -Math.sin(angle),Math.cos(angle),0,
+           0,0,1)
+    obj.position.applyMatrix3(m)
+
+    //坐标转换回世界坐标系
+    turnWorld(obj,target)
+    obj.lookAt(target)
+}
+
+function turnLocal(obj,target){
+    obj.position.x-=target.x;   
+    obj.position.y-=target.y;
+    obj.position.z-=target.z;
+}
+function turnWorld(obj,target){
+    obj.position.x+=target.x;   
+    obj.position.y+=target.y;
+    obj.position.z+=target.z;
+}
