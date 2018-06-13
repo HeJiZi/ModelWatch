39264c508e4d45015e385404316cf26198e29ced
HeJiZi <842236625@qq.com>
2018-06-13 11:29:21 +0800

    solve coflict

--- src/main/webapp/resources/js/modelControl.js
+++ src/main/webapp/resources/js/modelControl.js
@@ -4,8 +4,8 @@ function mouseSurround(event,target){
     x=event.clientX
     y=event.clientY
 
-    SurroundY(camera,target.position,dx/100.0)
-    SurroundZ(camera,target.position,-dy/100.0)
+    SurroundY(camera,target.position,dx/200.0)
+    SurroundByObjZ(camera,target.position,dy/300.0)
 
     renderer.render(scene,camera)
 }
@@ -46,6 +46,28 @@ function SurroundZ(obj,target,angle){
     obj.position.applyMatrix3(m)
 
     //坐标转换回世界坐标系
+    turnWorld(obj,target)
+    obj.lookAt(target)
+}
+function SurroundByObjZ(obj,target,angle){
+    turnLocal(obj,target)
+    var newup=new THREE.Vector3()
+    var d=new THREE.Vector3()
+    var newz=new THREE.Vector3()
+    d.subVectors(obj.position,target)
+    newup.copy(obj.up);
+    newz.crossVectors(newup,d);
+    newz.normalize();
+    var cos=Math.cos(angle)
+    var sin=Math.sin(angle)
+    var m=new THREE.Matrix3();
+    var nx=newz.x;
+    var ny=newz.y;
+    var nz=newz.z;
+    m.set(nx*nx*(1-cos)+cos,nx*ny*(1-cos)+nz*sin,nx*nz*(1-cos)-ny*sin,
+            nx*ny*(1-cos)-nz*sin,ny*ny*(1-cos)+cos,ny*nz*(1-cos)+nx*sin,
+            nx*nz*(1-cos)+ny*sin,ny*nz*(1-cos)-nx*sin,nz*nz*(1-cos)+cos)
+    obj.position.applyMatrix3(m)
     turnWorld(obj,target)
     obj.lookAt(target)
 }
