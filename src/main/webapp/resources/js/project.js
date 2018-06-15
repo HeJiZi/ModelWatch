779ab99f194c80c01ed2b26d4be3f47eebd5835a
zixiaoxiao <1070094159@qq.com>
2018-06-15 09:34:40 +0800

    做好了项目界面和主页

---
+++ src/main/webapp/resources/js/project.js
@@ -0,0 +1,14 @@
+const projects=new Vue({
+    el:"#a-middle",
+    data:{
+        projects:[
+            {name:'模型预览',time:'',preview_url:'../resources/images/9.jpeg'},
+            {name:'模型预览',time:'',preview_url:'../resources/images/5.jpeg'},
+            {name:'模型预览',time:'',preview_url:'../resources/images/12.jpeg'},
+            {name:'模型预览',time:'',preview_url:'../resources/images/temp.jpeg'},
+            {name:'模型预览',time:'',preview_url:'../resources/images/10.jpeg'},
+            {name:'模型预览',time:'',preview_url:'../resources/images/11.jpeg'},
+
+        ],
+    }
+})
---
+++ src/main/webapp/resources/js/show.js
@@ -0,0 +1,13 @@
+const projects=new Vue({
+    el:"#a-bottom",
+    data:{
+        projects:[
+            {name:'模型',time:'',preview_url:'../resources/images/6.jpeg'},
+            {name:'模型',time:'',preview_url:'../resources/images/7.jpeg'},
+            {name:'模型',time:'',preview_url:'../resources/images/9.jpeg'},
+            {name:'模型',time:'',preview_url:'../resources/images/5.jpeg'},
+            {name:'模型',time:'',preview_url:'../resources/images/8.jpeg'},
+            {name:'模型',time:'',preview_url:'../resources/images/desk.jpeg'},
+        ],
+    }
+})
