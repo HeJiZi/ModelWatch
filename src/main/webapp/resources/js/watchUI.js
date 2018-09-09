ca5eb133a68be66c007b59eb77c3ff1218c6bd6a
AColossus <842236625@qq.com>
2018-09-09 16:24:09 +0800

    完成了前端

--- src/main/webapp/resources/js/watchUI.js
+++ src/main/webapp/resources/js/watchUI.js
@@ -169,6 +169,24 @@ Vue.component('mw-photo', {
     `
 })
 
+Vue.component('mw-comment', {
+    inheritAttrs: false,
+    props: ['avater','name','content','time'],
+    template:
+        `
+        <div style="display: flex;padding-left: 20px;width: 80%;margin: 0 auto;align-items: center;justify-content: center">
+            <div  class="tempAvater" >
+                <img :src="avater" width="60px" height="60px">
+            </div>
+            <div style="width: 50%;">
+                <h5 style="margin: 10px 0px;">{{name}}<span style="color: #b9b9b9;">&nbsp;&nbsp;{{time}}</span></h5>
+                <p>{{content}}</p>
+            </div>
+        </div>
+    `
+})
+
+
 const vue = new Vue({
     el: '#nav',
     data: {
