eae86c8159a719b744292fc7281076d66a497d3f
SmacUL <1164253125@qq.com>
2018-12-08 09:39:47 +0800

    评论&回复 组件

--- front/src/WatchComponents/ModelWatchComment.vue
+++ front/src/WatchComponents/ModelWatchComment.vue
@@ -1,18 +1,212 @@
 <template>
-    <div style="display: flex;padding-left: 20px;width: 80%;margin: 0 auto;align-items: center;justify-content: center">
-        <div  class="tempAvater" >
-            <img :src="avater" width="60px" height="60px">
+    <div style="width: 50%; margin: 0 auto;">
+
+        <!-- 评论 -->
+        <div style="display: flex; padding-left: 20px; width: 100%;  margin-top: 20px; ">
+            <div  class="tempAvater" >
+                <img :src="avater" width="60px" height="60px">
+            </div>
+            <div style="width: 90%;">
+                <h5>{{name}}</h5>
+                <p class="contentP">{{content}}</p>
+                <div class="lastLineTip">
+                    <span class="timeSpan">&nbsp;&nbsp;{{time}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <el-button class="likeButton">赞</el-button>
+                    <span class="tipSpan" v-if="likeNum != 0">{{likeNum}}</span> <span class="tipSpan" v-if="likeNum == 0"></span> &nbsp;&nbsp;
+
+                    <el-button class="disLikeButton">踩</el-button>
+                    <span class="tipSpan" v-if="disLikeNum != 0">{{disLikeNum}}</span> <span class="tipSpan" v-if="disLikeNum == 0"></span> &nbsp;&nbsp;
+
+                    <el-button class="replyButton" @click="addReply(name, id, id)">回复</el-button>
+                    
+                </div>
+                
+            </div>
         </div>
-        <div style="width: 50%;">
-            <h5 style="margin: 10px 0px;">{{name}}<span style="color: #b9b9b9;">&nbsp;&nbsp;{{time}}</span></h5>
-            <p>{{content}}</p>
+        <!-- 回复 -->  
+        <div style="display: flex; padding-left: 20px; width: 83%; margin: 0 auto;" v-for="reply in replys" :key="reply.repId">
+            <div style="margin-top:20px" class="replyAvater">
+                <img :src="avater" width="40px" height="40px">
+            </div>
+            <div style="margin-top: 20px">
+                <div style="display: flex; width: 100%; ">
+                    <h5>{{reply.user.uUsername}}</h5><h5 style="color: #3299cc" v-if="reply.repUsername != ''">&nbsp;回复@</h5><h5 style="color: #3299cc">{{reply.repUsername}}</h5>
+                </div>
+                
+                <p class="contentP">{{reply.repContent}}</p>
+
+                <div class="lastLineTip">
+                    <span class="timeSpan">&nbsp;&nbsp;{{reply.repTime}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <el-button class="likeButton">赞</el-button>
+                    <span class="tipSpan" v-if="reply.repLikeNum != 0">{{reply.repLikeNum}}</span> <span class="tipSpan" v-if="reply.repLikeNum == 0"></span> &nbsp;&nbsp;
+
+                    <el-button class="disLikeButton">踩</el-button>
+                    <span class="tipSpan" v-if="reply.repDisLikeNum != 0">{{reply.repDisLikeNum}}</span> <span class="tipSpan" v-if="reply.repDisLikeNum == 0"></span> &nbsp;&nbsp;
+
+                    <el-button class="replyButton" @click="addReply(reply.user.uUsername, reply.repId, id)">回复</el-button>
+                    
+                </div>
+            </div>
         </div>
+
+
+        <div v-if="id == repCom && replyInputSeen == true">
+            <!-- 回复 panel -->
+            <div style="display: flex; padding-left: 20px;  padding-top: 20px; width: 100%;" >
+                <!-- 头像 -->
+                <div class="tempAvater" >
+                    <img :src="avater" width="60px" height="60px">
+                </div>
+
+                <!-- 回复输入框与按钮 panel -->
+                <div style="width:80%">
+                    <!-- 回复输入框 -->
+                    <el-input
+                            style="margin-left: 10px; width: 100%; border: 1px;"
+                            type="textarea"
+                            :rows="3"
+                            :placeholder="message"
+                            v-model="newReply"
+                    >
+                    </el-input>
+                    <!-- 发表评论与取消的按钮 -->
+                    <div id="button_panel" style="margin-top: 10px; margin-bottom: 10px">
+                        <el-button style="margin-left: 10px; padding: 4px" type="primary"  @click="showEmojiList">添加表情</el-button>
+                        <el-button style="margin-left: 10px; padding: 4px" type="primary"  @click="removeReply">取消评论</el-button>
+                    </div>
+
+                </div>
+                <!-- 发表评论的按钮 -->
+                <div style="margin-left: 20px; width: 10%;">
+                    <el-button style="padding: 4px; height: 75px; width: 100%; font-size: 13pt; line-height:24px;" type="primary"  @click="sendReply">发表<br>评论</el-button>
+                </div>
+            </div>
+            <!-- 表情包显示区 -->
+            <div style="margin-left: 80px; padding-left: 20px;">
+                <div style="" v-if="emojiListSeen == 1">
+                    <emoji-list v-on:emotion="addEmotionOnReply"></emoji-list>
+                </div>
+            </div>
+            
+        </div>
+
+
+
     </div>
 </template>
 
 <script>
+
+// emoji components
+import emojiList from '../pages/display/components/EmotionList.vue'
+
 export default {
+
+    data() {
+        return {
+            // replyInputSeen 判断回复框是否需要弹出的第二个条件（用户点击取消时使用）
+            replyInputSeen: false,
+            // emojiListSeen 
+            emojiListSeen: -1,
+
+            message: "回复 @ ", 
+            tarId: -1,
+            newReply: "",
+        }
+    },
+
+    created:function() {
+        
+    },
+
+    methods: {
+        // 回复按钮响应
+        // tarId 目标对象 id
+        // parId 回复所属的评论的 id
+        addReply(name, tarId, parId){
+            this.replyInputSeen = true;
+            this.emojiListSeen = -1;
+            this.tarId = tarId;
+            // alert(this.tarId);
+            this.message = "回复 @ " + name;
+            // 将回复按钮所属评论的 id 返回给父组件
+            this.$emit("replyButtonListener", parId);
+        },
+
+        removeReply() {
+            this.replyInputSeen = false;
+        },
+
+        sendReply() {
+
+        },
+
+        showEmojiList() {
+            this.emojiListSeen = -this.emojiListSeen
+        },
+
+        /**
+         * 回复内容添加表情包回显
+         */
+        addEmotionOnReply(data) {
+            this.newReply = this.newReply + data;
+        }
+    },
+
     inheritAttrs: false,
-    props: ['avater','name','content','time'],
+    // props: ['avater','name','content', 'id', 'time', 'replys', 'replyCom'] 
+    props: ['id', 'content', 'time', 'likeNum', 'disLikeNum', 'name', 'avater', 'replys', 'repCom'],
+
+
+    components: { emojiList },
+
 }
 </script>
+
+<style>
+
+h5 {
+    margin: 2px 0px 10px 10px;
+}
+
+.contentP {
+    margin: 0px 0px 5px 10px;
+}
+
+.timeSpan {
+    color: #b9b9b9;
+    
+}
+
+.tipSpan {
+    color: #b9b9b9;
+    display: inline-block;
+    width: 30px;
+}
+
+.lastLineTip {
+    font-size: 2pt
+}
+
+.replyButton {
+    width: 40px; 
+    height: 20px;
+    padding: 0px;
+    border: 0px;
+}
+
+.likeButton {
+    width: 20px; 
+    height: 20px;
+    padding: 0px;
+    border: 0px;
+}
+
+.disLikeButton {
+    width: 20px; 
+    height: 20px;
+    padding: 0px;
+    border: 0px;
+}
+
+</style>
--- front/src/pages/display/components/Interact.vue
+++ front/src/pages/display/components/Interact.vue
@@ -1,46 +1,111 @@
 <template>
-    <div style="background: #ffffff;">
-        <div style="display: flex;padding-top: 20px;padding-left: 20px;width: 80%;margin: 0 auto;align-items: center;justify-content: center">
-            <div  class="tempAvater" >
-                <img :src="user.uAvater" width="60px" height="60px">
+    <div style="background: #ffffff; padding-bottom: 20px;">
+        <div style="width: 60%; margin: 0 auto;">
+            <!-- 评论 panel -->
+            <div style="display: flex;padding-top: 20px; padding-left: 20px;width: 100%; justify-content: center">
+                <!-- 头像 -->
+                <div  class="tempAvater" >
+                    <img :src="user.uAvater" width="60px" height="60px">
+                </div>
+
+                <div style="width:80%">
+                    <!-- 评论输入框 -->
+                    <el-input
+                            style="width: 100%; border: 1px; margin-left: 10px;"
+                            type="textarea"
+                            :rows="3"
+                            :placeholder="message"
+                            v-model="newComments"
+                            >
+                    <!-- v-model="newComments" 这个是用来做什么的？ -->
+                    </el-input>
+                    
+                    <!-- 添加表情 -->
+                    <div id="button_panel" style="margin-top: 10px; margin-bottom: 10px">
+                        <el-button style=" margin-left: 10px; padding: 4px" type="primary"   @click="showEmojiList">添加表情</el-button>
+                    </div>
+                </div>
+                <!-- 发表评论的按钮 -->
+                <div style="margin-left: 20px; width: 10%;">
+                    <el-button style="padding: 4px; height: 75px; width: 100%; font-size: 13pt;line-height:24px;" type="primary" @click="review">发表<br>评论</el-button>
+                </div>
+                
+                
             </div>
-            <el-input
-                    style="width: 50%;"
-                    type="textarea"
-                    :rows="3"
-                    placeholder="请输入内容"
-                    v-model="newComments">
-            </el-input>
-            <el-button style="height: 80px;" type="primary" plain @click="review">发表评论</el-button>
+            <!-- 表情包显示区 -->
+            <div style="margin-left: 80px; padding-left: 20px;">    
+                <div style="" v-if="emojiListSeen == 1">
+                    <emoji-list v-on:emotion="addEmotionOnComment"></emoji-list>
+                </div>
+            </div>
+
         </div>
-        <mw-comment v-for="comment in comments" :name="comment.user.uUsername"
-                    :avater="comment.user.uAvater" :time="comment.comCreateTime"
-                    :content="comment.comContent"  :key="comment.id">
+        <!-- 评论&回复模板 -->
+        <mw-comment v-for="comment in comments" v-on:replyButtonListener="setRepCom" :key="comment.comId"
+                    :id="comment.comId"
+                    :content="comment.comContent"
+                    :time="comment.comCreateTime"
+                    :likeNum="comment.comLikeNum"
+                    :disLikeNum="comment.comDisLikeNum"
+                    :name="comment.user.uUsername"
+                    :avater="comment.user.uAvater" 
+                     
+                    :replys="comment.replys"
+
+                    :repCom="repCom" 
+                    >
         </mw-comment>
+
+
     </div>
 </template>
 
 
 <script>
 import mwComment from '@/WatchComponents/ModelWatchComment.vue'
+
+// emoji components
+import emojiList from './EmotionList.vue'
+
 export default {
     data(){
         return{
+            emojiListSeen: -1,
+
+            // replyCom 用于提示子组件当前需要显示回复框的评论编号
+            repCom: -1,
+
+            // newCommentContent: "",
+            message: "请输入内容",
+
+            // 新评论 (数据还没有整理)
             newComments:'',
             user:{
                 uAvater:'/static/images/small_logo.png',
                 uId:'',
             },
-            comments:[{
-                id:1,
-                user:{
-                    uUsername:'王虎',
-                    uAvater:'/static/images/small_logo.png'
-                },
-                comUid:'1',
-                comContent:'这模型做得真精致',
-                comCreateTime:'2011-1-1',
+
+            // 评论列表
+            comments: [{
+                comId: 1, comContent: '这看起来真棒啊', comCreateTime: '2017-09-21 08:12:23', comLikeNum: 10, comDisLikeNum: 0, 
+                user: {uUsername: '隔壁老王', uAvater: '/static/images/small_logo.png'},
+                replys: [{
+                    repId: 21, repContent: '是啊是啊', repTime: '2017-09-21 09:12:23', repLikeNum: 2, repDisLikeNum: 0, repUsername: '',
+                    user: {uUsername: '小孙', uAvater: '/static/images/small_logo.png'}
+                }, {
+                    repId: 24, repContent: '放屁', repTime: '2017-09-21 10:12:23', repLikeNum: 0, repDisLikeNum: 12, repUsername: '小孙',
+                    user: {uUsername: '傻屌', uAvater: '/static/images/small_logo.png'}
+                }, {
+                    repId: 25, repContent: '你才放屁', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '傻屌',
+                    user: {uUsername: '舞法天女', uAvater: '/static/images/small_logo.png'}
+                }]
+            }, {
+                comId: 2, comContent: '编不下去了', comCreateTime:  '2016-09-21 08:12:23', comLikeNum: 12, comDisLikeNum: 0,
+                user: {uUsername: '钢铁侠', uAvater: '/static/images/small_logo.png'},
+                replys: []
             }]
+
+
         }
     },
     created:function() {
@@ -67,9 +132,31 @@ export default {
                 if(response.data == true) alert("评论成功，请等待管理员审核");
                 else alert("评论失败，存在异常操纵")
             })
-        }
+        },
+
+        /**
+         * 设置当前点击了回复按钮的评论 id
+         */
+        setRepCom(data) {
+            this.repCom = data;
+        },
+
+        /**
+         * 控制是否显示表情包显示区
+         */
+        showEmojiList() {
+            this.emojiListSeen = -this.emojiListSeen
+        },
+
+        /**
+         * 评论内容添加表情包回显
+         */
+        addEmotionOnComment(data) {
+            this.newComments = this.newComments + data;
+        },
+
     },
-    components:{ mwComment }, 
+    components:{ mwComment, emojiList }, 
 }
 </script>
 
