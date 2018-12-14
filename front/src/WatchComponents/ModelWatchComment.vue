ee3baa295a77f315da67b9328fb056c52a659633
SmacUL <1164253125@qq.com>
2018-12-14 11:45:58 +0800

    评论&回复模块 二次修改
    回复展示改为分页
    页面布局修改

--- front/src/WatchComponents/ModelWatchComment.vue
+++ front/src/WatchComponents/ModelWatchComment.vue
@@ -1,107 +1,119 @@
 <template>
-    <div style="width: 53%; margin: 0 auto;">
-
+    <div style="width: 60%; margin: 0 auto;">
         <!-- 评论 -->
         <div style="display: flex;  width: 100%;  margin-top: 20px; ">
-            <div  class="tempAvater" >
-                <img :src="avater" width="60px" height="60px">
+            <div  class="tempAvater" style="margin-right: 30px">
+                <img :src="avater" width="70px" height="70px">
             </div>
             <div style="width: 90%;">
-                <h5>{{name}}</h5>
-                <p class="contentP">{{content}}</p>
-                <div class="lastLineTip">
-                    <span class="timeSpan">&nbsp;&nbsp;{{time}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <el-button class="likeButton">赞</el-button>
-                    <span class="tipSpan" v-if="likeNum != 0">{{likeNum}}</span> 
-                    <span class="tipSpan" v-if="likeNum == 0"></span> &nbsp;&nbsp;
-
-                    <el-button class="disLikeButton">踩</el-button>
-                    <span class="tipSpan" v-if="disLikeNum != 0">{{disLikeNum}}</span> 
-                    <span class="tipSpan" v-if="disLikeNum == 0"></span> &nbsp;&nbsp;
-
-                    <el-button class="replyButton" @click="addReply(name, id, id, 0)">
-                        <span v-if="needRepId == id && needRepType == 0 && needRepParId == repCom">取消</span>回复
-                    </el-button>
-                    
-                </div>
-            </div>
-        </div>
-
-        <div style="margin: 0 auto; width: 85%">
-            <e-input-panel 
-                :avater="avater", :repId="id", 
-                :needRepId="needRepId", :keyType="0", 
-                :needRepType="needRepType", :comId="id", 
-                :repCom="repCom", :message="inputTip",
-                :newComments="newComments" 
-                >
-            </e-input-panel>
-        </div>
-
-        <!-- 循环显示回复及回复面板 -->  
-        <div v-if="index < needShowReplyNum" v-for="(reply, index) in replys" 
-            :key="reply.repId">
-
-            <div style="display: flex; width: 85%; margin: 0 auto;">
-                <div style="margin-top:20px" class="replyAvater">
-                    <img :src="avater" width="40px" height="40px">
-                </div>
-                <div style="margin-top: 20px">
-                    <div style="display: flex; width: 100%; ">
-                        <h5>{{reply.user.uUsername}}</h5>
-                        <h5 style="color: #409EFF" v-if="reply.repUsername != ''">&nbsp;回复@</h5>
-                        <h5 style="color: #409EFF">{{reply.repUsername}}</h5>
-                    </div>
-                    
-                    <p class="contentP">{{reply.repContent}}</p>
-
+                <!-- 评论内容 -->
+                <div>
+                    <h5>{{name}}</h5>
+                    <p class="contentP">{{content}}</p>
                     <div class="lastLineTip">
-                        <span class="timeSpan">&nbsp;&nbsp;{{reply.repTime}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <span class="timeSpan">&nbsp;&nbsp;{{time}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
                         <el-button class="likeButton">赞</el-button>
-                        <span class="tipSpan" v-if="reply.repLikeNum != 0">{{reply.repLikeNum}}</span> 
-                        <span class="tipSpan" v-if="reply.repLikeNum == 0"></span> &nbsp;&nbsp;
+                        <span class="tipSpan" v-if="likeNum != 0">{{likeNum}}</span> 
+                        <span class="tipSpan" v-if="likeNum == 0"></span> &nbsp;&nbsp;
 
                         <el-button class="disLikeButton">踩</el-button>
-                        <span class="tipSpan" v-if="reply.repDisLikeNum != 0">{{reply.repDisLikeNum}}</span>
-                        <span class="tipSpan" v-if="reply.repDisLikeNum == 0"></span> &nbsp;&nbsp;
+                        <span class="tipSpan" v-if="disLikeNum != 0">{{disLikeNum}}</span> 
+                        <span class="tipSpan" v-if="disLikeNum == 0"></span> &nbsp;&nbsp;
 
-                        <el-button class="replyButton" @click="addReply(reply.user.uUsername, reply.repId, id, 1)">
-                            <span v-if="needRepId == reply.repId && needRepType == 1 && needRepParId == repCom">取消</span>回复
+                        <el-button class="replyButton" @click="addReply(name, id, id, 0)">
+                            <span v-if="needRepId == id && needRepType == 0 && needRepParId == repCom">取消</span>回复
                         </el-button>
-                        
                     </div>
                 </div>
-            </div>
+                <!-- 评论输入面板 -->
+                <div style="width: 100%">
+                    <e-input-panel 
+                        :user="user", :repId="id",
+                        :needRepId="needRepId", :keyType="0", 
+                        :needRepType="needRepType", :comId="id", 
+                        :repCom="repCom", :inputTip="inputTip",
+                        >
+                    </e-input-panel>
+                </div>
+                <!-- 循环显示回复及回复面板 -->  
+                <div v-if="index > startShowReplyIndex && index < endShowReplyIndex" v-for="(reply, index) in replys" 
+                    :key="reply.repId">
+                    <!-- 回复内容 -->
+                    <div style=" width: 100%; display: flex; ">
+                        <!-- 头像 -->
+                        <div style="margin-top:20px; margin-right: 10px" class="replyAvater">
+                            <img :src="avater" width="40px" height="40px">
+                        </div>
+                        <div>
+                            <div style="margin-top: 20px">
+                                <div style="display: flex; width: 100%; ">
+                                    <h5>{{reply.user.uUsername}}</h5>
+                                    <h5 style="color: #409EFF" v-if="reply.repUsername != ''">&nbsp;回复@</h5>
+                                    <h5 style="color: #409EFF">{{reply.repUsername}}</h5>
+                                </div>
+                                
+                                <p class="contentP">{{reply.repContent}}</p>
 
-            <div style="margin: 0 auto; width: 85%">
-                <e-input-panel 
-                    :avater="avater", :repId="reply.repId", 
-                    :needRepId="needRepId", :keyType="1", 
-                    :needRepType="needRepType", :comId="id", 
-                    :repCom="repCom", :message="inputTip",
-                    :newComments="newComments"
-                    >
-                </e-input-panel>
-            </div>
+                                <div class="lastLineTip">
+                                    <span class="timeSpan">&nbsp;&nbsp;{{reply.repTime}}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <el-button class="likeButton">赞</el-button>
+                                    <span class="tipSpan" v-if="reply.repLikeNum != 0">{{reply.repLikeNum}}</span> 
+                                    <span class="tipSpan" v-if="reply.repLikeNum == 0"></span> &nbsp;&nbsp;
 
+                                    <el-button class="disLikeButton">踩</el-button>
+                                    <span class="tipSpan" v-if="reply.repDisLikeNum != 0">{{reply.repDisLikeNum}}</span>
+                                    <span class="tipSpan" v-if="reply.repDisLikeNum == 0"></span> &nbsp;&nbsp;
+
+                                    <el-button class="replyButton" @click="addReply(reply.user.uUsername, reply.repId, id, 1)">
+                                        <span v-if="needRepId == reply.repId && needRepType == 1 && needRepParId == repCom">取消</span>回复
+                                    </el-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 针对回复的输入面板 -->
+                    <div style=" width: 100%">
+                        <e-input-panel 
+                            :user="user", :repId="reply.repId", 
+                            :needRepId="needRepId", :keyType="1", 
+                            :needRepType="needRepType", :comId="id", 
+                            :repCom="repCom", :inputTip="inputTip",
+                            >
+                        </e-input-panel>
+                    </div>
+                </div>
+                <!-- 分页 -->
+                <div style=" margin: 0 auto; display: flex; justify-content: center">
+                    <el-pagination 
+                        v-if="replys.length > this.EXTENDSHOWREPLYNUM && controlTipButtonSeen == -1"
+                        :page-size="this.EXTENDSHOWREPLYNUM"
+                        layout="prev, pager, next"
+                        :total="replys.length"
+                        :current-page.sync="page"
+                        @next-click="nextPage"
+                        @prev-click="prevPage"
+                        @current-change="changePage"
+                        >
+                    </el-pagination>
+                </div>
+                <!-- 点击按钮显示更多评论 -->
+                <div style="justify-content: center; display: flex; margin-top: 10px">
+                    <el-button plain icon="el-icon-arrow-down" style=" width: 200px; border: 0px; color:#409EFF"
+                        v-if="replys.length > this.BASESHOWREPLYNUM && controlTipButtonSeen == 1"
+                        @click="showReplyControl(1)">
+                        <span style="color:#409EFF;">点击显示更多回复</span>
+                    </el-button>
+                    <el-button plain icon="el-icon-arrow-up" style=" width: 200px; border: 0px; color:#409EFF" 
+                        v-if="controlTipButtonSeen == -1"
+                        @click="showReplyControl(-1)">
+                        <span style="color:#409EFF;">点击收回所有回复</span>
+                    </el-button>
+                </div>
+
+            </div>
         </div>
 
         <br>
-
-        <!-- 点击按钮显示更多评论 -->
-        <div style="justify-content: center; display: flex;" >
-            <el-button plain icon="el-icon-arrow-up" style=" width: 200px; border: 0px; color:#409EFF" 
-                
-                v-if="needShowReplyNum > 3" 
-                @click="showReplyControl(-1)">
-                <span style="color:#409EFF;">点击收回所有回复</span>
-            </el-button>
-            <el-button plain icon="el-icon-arrow-down" style=" width: 200px; border: 0px; color:#409EFF" 
-                v-if=" replys.length - needShowReplyNum >= 1"
-                @click="showReplyControl(1)">
-                <span style="color:#409EFF;">点击显示更多回复</span>
-            </el-button>
-        </div>
 
     </div>
 </template>
@@ -111,25 +123,34 @@
 import eInputPanel from '../pages/display/components/InputPanel.vue'
 
 export default {
+    props: ['user', 'id', 'content', 'time', 'likeNum', 'disLikeNum', 'name', 'avater', 'replys', 'repCom'],
 
     data() {
         return {
-            tarId: -1,
-            newReply: "",
-
-            // 显示回复框用
+            // tarId: -1,
+            // newReply: "",
+            // @needRepParId 需要显示回复编辑框的评论 id
             needRepParId: -1,
+            // @needRepId 需要显示回复编辑框的回复 id
             needRepId: -1,
+            // @needRepType 用于确定需要显示回复编辑框的是评论（0）还是回复（1）
             needRepType: -1,
-
-            // 输入框显示提示信息
+            // @inputTip 输入框顶部的提示信息
             inputTip: "回复@ ",
-
-            // 测试 控制需要显示的回复数量
-            needShowReplyNum: 3,
+            // @BASESHOWREPLYNUM 最初每条评论下需要展示的回复数量
+            BASESHOWREPLYNUM: 2,
+            // @EXTENDSHOWREPLYNUM 点击展开回复按钮之后每条评论下需要显示的回复数量（每页的回复数量）
+            EXTENDSHOWREPLYNUM: 6,
+            // @page 分页时确定页数
+            page: 1,
+            // @startShowReplyIndex 需要显示的回复的开始下标
+            startShowReplyIndex: -1,
+            // @endShowReplyIndex 需要显示的回复的结束下标
+            endShowReplyIndex: 2,
+            // @controlTipButtonSeen 用于控制显示 '点击显示更多回复' 还是 '点击收回所有回复' 按钮
+            controlTipButtonSeen: 1,
         }
     },
-
 
     methods: {
         /**
@@ -159,21 +180,36 @@ export default {
         showReplyControl(signal) {
 
             if (signal == 1) {
-                this.needShowReplyNum = this.needShowReplyNum + 3;
+                this.endShowReplyIndex = this.EXTENDSHOWREPLYNUM;
             } else {
-                this.needShowReplyNum = 3;
+                this.endShowReplyIndex = this.BASESHOWREPLYNUM;
+                this.startShowReplyIndex = -1;
                 // 收回回复的时候顺手把评论框关掉
                 this.needRepId = -1;
                 this.needRepType = -1;
                 this.needRepParId = -1;
+
             }
-            
+            this.controlTipButtonSeen = - this.controlTipButtonSeen;
         },
 
+        nextPage() {
+            this.startShowReplyIndex = this.startShowReplyIndex + this.EXTENDSHOWREPLYNUM;
+            this.endShowReplyIndex = this.endShowReplyIndex + this.EXTENDSHOWREPLYNUM;
+        },
+
+        prevPage() {
+            this.startShowReplyIndex = this.startShowReplyIndex - this.EXTENDSHOWREPLYNUM;
+            this.endShowReplyIndex = this.endShowReplyIndex - this.EXTENDSHOWREPLYNUM;
+        },
+
+        changePage() {
+            this.startShowReplyIndex = (this.page - 1) * this.EXTENDSHOWREPLYNUM - 1;
+            this.endShowReplyIndex = this.page * this.EXTENDSHOWREPLYNUM;
+        }
     },
 
     inheritAttrs: false,
-    props: ['id', 'content', 'time', 'likeNum', 'disLikeNum', 'name', 'avater', 'replys', 'repCom', 'newComments'],
     components: { eInputPanel },
 
 }
@@ -183,6 +219,10 @@ export default {
 
 h5 {
     margin: 2px 0px 10px 10px;
+}
+
+hr {
+    color:#e7e7e7;
 }
 
 .contentP {
--- front/src/pages/display/components/Interact.vue
+++ front/src/pages/display/components/Interact.vue
@@ -1,17 +1,18 @@
 <template>
     <div style="background: #ffffff; padding-bottom: 20px;">
+        <!-- 顶部评论输入框 -->
         <div style="width: 60%; margin: 0 auto;">
             <e-input-panel 
-                :avater="user.uAvater", :repId="0", 
+                :user="user", :repId="0", 
                 :needRepId="0", :keyType="0", 
                 :needRepType="0", :comId="0", 
-                :repCom="0", :message="inputTip",
-                :newComments="newComments">
+                :repCom="0", :inputTip="inputTip",
+                >
             </e-input-panel>
         </div>
-
         <!-- 评论&回复模板 -->
         <mw-comment v-for="comment in comments" v-on:replyButtonListener="setRepCom" :key="comment.comId"
+                    :user="user"
                     :id="comment.comId"
                     :content="comment.comContent"
                     :time="comment.comCreateTime"
@@ -19,17 +20,23 @@
                     :disLikeNum="comment.comDisLikeNum"
                     :name="comment.user.uUsername"
                     :avater="comment.user.uAvater" 
-                     
                     :replys="comment.replys"
                     :repCom="repCom"
                     >
         </mw-comment>
         <br>
-        <div style=" margin: 0 auto; display: flex; justify-content: center">
-            <el-pagination v-if="commentsInfo.totalNum >= 8"    
-                :page-size="8"
+        <!-- 分页 -->
+        <div style="width: 60%; margin: 0 auto;">
+            <el-pagination v-if="commentsInfo.totalNum >= commentsInfo.pageSize"    
+                background
+                :page-size="commentsInfo.pageSize"
                 layout="prev, pager, next"
-                :total="commentsInfo.totalNum">
+                :total="commentsInfo.totalNum"
+                :current-page.sync="commentsInfo.curPage"
+                @next-click="nextPage"
+                @prev-click="prevPage"
+                @current-change="changePage"
+                >
             </el-pagination>
         </div>
     </div>
@@ -38,35 +45,29 @@
 
 <script>
 import mwComment from '@/WatchComponents/ModelWatchComment.vue'
-
 import eInputPanel from './InputPanel.vue'
 
-
 export default {
-    data(){
+    data() {
         return{
-            // emojiListSeen: -1,
-
-            // replyCom 用于提示子组件当前需要显示回复框的评论编号
+            // @replyCom 用于提示子组件当前需要显示回复框的评论编号
             repCom: -1,
-
-            // newCommentContent: "",
+            // @inputTip 输入框顶部的提示信息
             inputTip: "请自觉遵守互联网相关的法律法规，文明发表评论！",
-
-            // 新评论 (数据还没有整理)
-            newComments:'',
+            // @newComments 新评论内容（可能要被废除掉）
+            // newComments:'',
+            // @user 当前用户的信息
             user:{
                 uAvater:'/static/images/small_logo.png',
                 uId:'',
             },
-
-            // 评论信息 用于分页
+            // @commentsInfo 评论信息（评论总数，当前页面） 用于分页
             commentsInfo: {
                 totalNum: 200,
                 curPage: 1,
+                pageSize: 8,
             },
-
-            // 评论列表
+            // @comments 列表 评论回复，测试用
             comments: [{
                 comId: 1, comContent: '这看起来真棒啊', comCreateTime: '2017-09-21 08:12:23', comLikeNum: 10, comDisLikeNum: 0, 
                 user: {uUsername: '隔壁老王', uAvater: '/static/images/small_logo.png'},
@@ -126,11 +127,30 @@ export default {
                     repId: 40, repContent: 'this is a test', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '',
                     user: {uUsername: '40', uAvater: '/static/images/small_logo.png'}
                 }]
+            }, {
+                comId: 3, comContent: '编不下去了', comCreateTime:  '2016-09-21 08:12:23', comLikeNum: 12, comDisLikeNum: 0,
+                user: {uUsername: '钢铁侠', uAvater: '/static/images/small_logo.png'},
+                replys: []
+            }, {
+                comId: 4, comContent: '编不下去了', comCreateTime:  '2016-09-21 08:12:23', comLikeNum: 12, comDisLikeNum: 0,
+                user: {uUsername: '钢铁侠', uAvater: '/static/images/small_logo.png'},
+                replys: [{
+                    repId: 41, repContent: 'this is a test', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '',
+                    user: {uUsername: '41', uAvater: '/static/images/small_logo.png'}
+                }, {
+                    repId: 42, repContent: 'this is a test', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '',
+                    user: {uUsername: '42', uAvater: '/static/images/small_logo.png'}
+                }, {
+                    repId: 43, repContent: 'this is a test', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '',
+                    user: {uUsername: '43', uAvater: '/static/images/small_logo.png'}
+                }, {
+                    repId: 44, repContent: 'this is a test', repTime: '2017-09-21 10:18:23', repLikeNum: 10, repDisLikeNum: 0, repUsername: '',
+                    user: {uUsername: '44', uAvater: '/static/images/small_logo.png'}
+                }]
             }]
-
-
         }
     },
+
     created:function() {
         //Todo:ajax获取评论数据
 
@@ -144,21 +164,23 @@ export default {
         // })
     },
 
-
-
     methods:{
-        review(){
-            var url = window.location.href;
-            var id = url.substr(url.lastIndexOf('/') + 1, url.length);
-            this.$http.post('/model/comment',{
-                comContent:this.newComments,
-                comUId:this.user.uId,
-                comMId:id,
-            }).then((response) => {
-                if(response.data == true) alert("评论成功，请等待管理员审核");
-                else alert("评论失败，存在异常操纵")
-            })
-        },
+        /**
+         * 评论审查
+         * 这个方法已经被转移到 InputPanel 组件
+         */
+        // review(){
+        //     var url = window.location.href;
+        //     var id = url.substr(url.lastIndexOf('/') + 1, url.length);
+        //     this.$http.post('/model/comment',{
+        //         comContent:this.newComments,
+        //         comUId:this.user.uId,
+        //         comMId:id,
+        //     }).then((response) => {
+        //         if(response.data == true) alert("评论成功，请等待管理员审核");
+        //         else alert("评论失败，存在异常操纵")
+        //     })
+        // },
 
         /**
          * 设置当前点击了回复按钮的评论 id
@@ -167,15 +189,35 @@ export default {
             this.repCom = data;
         },
 
+        nextPage() {
+            // 测试用
+            this.comments = [{
+                comId: 7, comContent: '编去了', comCreateTime:  '2016-09-21 08:12:23', comLikeNum: 12, comDisLikeNum: 0,
+                user: {uUsername: '钢侠', uAvater: '/static/images/small_logo.png'},
+                replys: []
+            }]
+        },
+
+        prevPage() {
+
+        },
+
+        changePage() {
+       
+        }
+
     },
+
     components:{ mwComment, eInputPanel }, 
+
 }
+
 </script>
 
 <style>
     .tempAvater{
-        width: 60px;
-        height: 60px;
+        width: 80px;
+        height: 80px;
         float: left;
         border-radius: 50%;
         border: 2px solid white;
